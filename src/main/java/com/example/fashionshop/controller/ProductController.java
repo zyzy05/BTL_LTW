29d093ed3ef4179b0d30279fe97520026d4b67ca
duahauTLE <hoangcoihd6@gmail.com>
2026-03-07 10:05:36 +0700

    feat: hien thi thong tin chi tiet san pham

--- src/main/java/com/example/fashionshop/controller/ProductController.java
+++ src/main/java/com/example/fashionshop/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,17 @@ public class ProductController {
     public ResponseEntity<?> getAllProducts() {
         ResponseData responseData = new ResponseData();
         responseData.setData(productService.getAllProducts());
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    // hiển thị chi tiết san pham
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> getProductDetail (@PathVariable long id)
+    {
+
+        ResponseData responseData = new ResponseData();
+        responseData.setData(productService.getProductDetail(id));
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
--- src/main/java/com/example/fashionshop/service/ProductService.java
+++ src/main/java/com/example/fashionshop/service/ProductService.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
-
+    // hiển thi toàn bộ sản phẩm
     public List<ProductsResponse> getAllProducts() {
 
         List<Product> products = productRepository.findAll();
@@ -61,5 +61,51 @@ public class ProductService {
 
 
         return productsResponses;
+    }
+
+    // hiển thị chi tiết san pham
+    public ProductsResponse getProductDetail(long id) {
+        Product product = productRepository.findById(id).orElse(null);
+        ProductsResponse productsResponse = new ProductsResponse();
+
+        productsResponse.setName(product.getName());
+        productsResponse.setPrice(product.getPrice());
+        productsResponse.setDescription(product.getDescription());
+
+        ProductsResponse request = new ProductsResponse();
+
+        request.setName(product.getName());
+        request.setPrice(product.getPrice());
+        request.setDescription(product.getDescription());
+        request.setPrice(product.getPrice());
+
+
+        List<ProductImageResponse> productImageResponses = new ArrayList<>();
+        for (ProductImage image : product.getImages()) {
+            ProductImageResponse productImageResponse = new ProductImageResponse();
+            productImageResponse.setImageUrl(image.getImageUrl());
+            productImageResponses.add(productImageResponse);
+
+        }
+        List<ProductVariantResponse> productVariantResponses = new ArrayList<>();
+        for (ProductVariant variant : product.getVariants()) {
+            ProductVariantResponse productVariantResponse = new ProductVariantResponse();
+            Size size = variant.getSize();
+            SizeResponse sizeResponse = new SizeResponse();
+            sizeResponse.setName(size.getName());
+
+            productVariantResponse.setSize(sizeResponse);
+            productVariantResponse.setStockQuantity(variant.getStockQuantity());
+            productVariantResponses.add(productVariantResponse);
+        }
+
+        request.setProductVariants(productVariantResponses);
+        request.setImages(productImageResponses);
+
+
+
+
+
+        return request;
     }
 }
