6ad83191598080f0ddab08d0b0bf856a4858d1a0
duahauTLE <hoangcoihd6@gmail.com>
2026-03-09 23:41:02 +0700

    feat: phan quyen khach vang lai

---
+++ src/main/java/com/example/fashionshop/config/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.example.fashionshop.config;
+
+import com.example.fashionshop.entity.User;
+import com.example.fashionshop.repository.UserRepository;
+import com.example.fashionshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(username ,"{noop}" + user.getPassword(),new ArrayList<>());
+    }
+}
--- src/main/java/com/example/fashionshop/config/SecurityConfig.java
+++ src/main/java/com/example/fashionshop/config/SecurityConfig.java
@@ -1,20 +1,30 @@
 package com.example.fashionshop.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()   // cấp full quyền
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/products/**").permitAll()
+                        .anyRequest().authenticated()
+
                 );
+
+
 
         return http.build();
     }
